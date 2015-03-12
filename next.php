b05b6f90d39767cd8695e3075c144db5342c4e64
Lewis Park <lewispark@hotmail.com>
2015-03-12 13:54:52 +0000

    Working DB PHP Query
    PHP will query relations table now, prior to adding JSON encoding

---
+++ next.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<?php
+
+$yourname = $_REQUEST['yourname'];
+
+$searchterm = $_REQUEST['searchterm'];
+
+?>
+
+<body>
+search was <?php print $searchterm ?>
+</body>
+</html>
